9ec3d0bc15c564c19b523361670ed43675eb8ac3
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-08-27 21:20:22 +0600

    basic start with the basic Function

---
+++ basic/a_Function/a_basicFunction.java
@@ -0,0 +1,11 @@
+package a_Function;
+public class a_basicFunction {
+
+    public static void main(String[] args) {
+        func();
+    }
+
+    public static void func() {
+        System.out.println("Inside Function");
+    }
+}
