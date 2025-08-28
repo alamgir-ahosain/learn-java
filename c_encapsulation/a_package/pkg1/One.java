1668f46073571cc226342ba21d139ad47453c3e4
alamgir-ahosain <alamgir.ahosain@gmail.com>
2025-08-28 23:14:32 +0600

    create package

---
+++ c_encapsulation/a_package/pkg1/One.java
@@ -0,0 +1,18 @@
+
+
+package c_encapsulation.a_package.pkg1;
+import c_encapsulation.a_package.pkg2.Two;
+
+public class One {
+    public static void main(String[] args) {
+
+
+        /*
+        Two t=new Two("Alamgir"); if constructor not declared as public 
+             constructor can be accessed only from within the same package.
+            It cannot be accessed from other packages, even if the class itself is public.
+         */
+        Two t=new Two("Alamgir"); //work only  if constructor Two(..) is public
+    
+    }
+}
